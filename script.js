146c38596153aa8dee8f814fad8b2a00ecba29fc
Stefan Weidner <stephan721@gmx.net>
2025-09-26 09:39:21 +0200

    new organisation

---
+++ script.js
@@ -0,0 +1,28 @@
+let myDishes = [
+  {
+    name: "Spaghetti Carbonara",
+    price: 12.99,
+    description: "Cremige Pasta mit Speck und Parmesan, verfeinert mit Sahne.",
+  },
+  {
+    name: "Pizza Margherita",
+    price: "9.50",
+    description: "Standard Pizza mit allem was dazu gehört.",
+  },
+  {
+    name: "Pizza Diavola",
+    price: "12.99",
+    description: "Cremige Pasta mit Speck und Parmesan, verfeinert mit Sahne.",
+  },
+  {
+    name: "Spaghetti Carbonara",
+    price: "12.99",
+    description: "Cremige Pasta mit Speck und Parmesan, verfeinert mit Sahne.",
+  },
+  {
+    name: "Spaghetti Carbonara",
+    price: "12.99",
+    description: "Cremige Pasta mit Speck und Parmesan, verfeinert mit Sahne.",
+  },
+];
+/* amount = 0 und dann Hochzählen oder img hinzufügen */
